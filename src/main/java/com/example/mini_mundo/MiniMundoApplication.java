085f14b7d1e3a1b17448ddcbd40cb9c1fad5be73
Alison Souza <71190247+Alison561@users.noreply.github.com>
2025-05-07 14:51:01 -0300

    first commit

---
+++ src/main/java/com/example/mini_mundo/MiniMundoApplication.java
@@ -0,0 +1,13 @@
+package com.example.mini_mundo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniMundoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniMundoApplication.class, args);
+	}
+
+}
